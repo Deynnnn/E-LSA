6200fe27346b529034b2bb194e60ee7701a10add
Rhoeldain Pascua <deyndeyn727@gmail.com>
2024-03-21 22:39:36 +0800

    profile update

--- login.php
+++ login.php
@@ -47,7 +47,6 @@
     </div>
     <div class="loader"></div>
     <script src="./js/loader.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <?php require("./includes/footer.php"); ?>
 </body>
 </html>
